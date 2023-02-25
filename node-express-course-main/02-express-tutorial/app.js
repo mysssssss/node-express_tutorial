aac9a70377128112c3d10d54b2d3437c22fb67e3
mysssssss <danila.2565@gmail.com>
2023-02-25 10:58:56 -0500

    t

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+// req => middleware => res
+app.use(logger, authorize);
+
+app.get('/', (req, res) => {
+  res.send('home page');
+});
+app.get('/about', (req, res) => {
+  res.send('about page');
+});
+app.get('/api/products', (req, res) => {
+  res.send('products page');
+});
+app.get('/api/items', (req, res) => {
+  console.log(req.user);
+  res.send('items page');
+});
+
+app.listen(5000, () => {
+  console.log('listening on port: 5000');
+});
