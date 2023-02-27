765509c69beee2fce4fed2f1ecb9885ab428bc50
mysssssss <danila.2565@gmail.com>
2023-02-26 20:08:53 -0500

    started 03

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
+app.use(express.static('./public'));
+
+// routes
+app.get('/', (req, res) => {
+  res.send(index);
+});
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
