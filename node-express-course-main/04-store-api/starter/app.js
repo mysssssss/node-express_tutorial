7b7f5feda1db15a097307ff1b33db5da5b706357
mysssssss <danila.2565@gmail.com>
2023-03-05 11:09:57 -0500

    started 4

--- node-express-course-main/04-store-api/starter/app.js
+++ node-express-course-main/04-store-api/starter/app.js
@@ -1,1 +1,34 @@
-console.log('04 Store API')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleWare = require('./middleware/error-handler');
+
+// async errors
+
+// routes
+app.get('/', (req, res) => {
+  res.send(
+    '<h1>home page</h1> <br>  <a href="/api/v1/products">go here for products page</a>'
+  );
+});
+
+// products route
+
+app.use(notFoundMiddleware);
+app.su;
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  // connect db
+  try {
+    app.listen(port, () => {
+      console.log(`app listening on port: ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
