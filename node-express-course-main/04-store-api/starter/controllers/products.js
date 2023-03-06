d67de156f1d3726fc809a1050b23de4ecc6da543
mysssssss <danila.2565@gmail.com>
2023-03-05 22:46:49 -0500

    yes

--- node-express-course-main/04-store-api/starter/controllers/products.js
+++ node-express-course-main/04-store-api/starter/controllers/products.js
@@ -2,14 +2,28 @@ const { products } = require('../../../02-express-tutorial/data');
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
+  const search = 'ab';
   const products = await Product.find({
-    name: 'vase table',
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products route' });
+  const { featured, name, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const product = await Product.find(queryObject);
+  res.status(200).json({ nbHits: product.length, product });
 };
 
 module.exports = {
